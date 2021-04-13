3bf5b301764f8961de26d9298edec232db6903e7
sanjeeb subedi <sanjeebsubedi4@gmail.com>
2021-04-13 21:52:33 +0545

    details endpoint enhanced

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import base64
 import io
 from PIL import Image
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, json, render_template, request, jsonify
 from tensorflow.keras.models import load_model
 import cv2
 import numpy as np
@@ -9,13 +9,15 @@ import psycopg2
 
 app = Flask(__name__)
 
-indices = {0: 'Catweed', 1: 'Guava', 2: 'Jackfruit', 3: 'Lychee', 4: 'Potato', 5: 'Rose'}
+# indices = {0: 'Catweed', 1: 'Guava', 2: 'Jackfruit', 3: 'Lychee', 4: 'Potato', 5: 'Rose'}
 # indices = {0: 'Indian_rhododendron', 1: 'Catweed', 2: 'Guava', 3: 'Barberry', 4: 'Jackfruit', 5: 'Mulberry', 6: 'Chestnut', 7: 'Lychee', 8: 'Orange', 9: 'Mint', 10: 'Potato', 11: 'Rose'}
+indices = {0: 'barberry', 1: 'catweed', 2: 'chestnut', 3: 'guava', 4: 'indian_rhododendron', 5: 'jackfruit', 6: 'lychee', 7: 'mulberry', 8: 'orange', 9: 'potato', 10: 'rose', 11: 'spearmint'}
 
 def get_model():
     global model
-    model_name = 'mobilenet_model_6_classes.h5'
+    # model_name = 'mobilenet_model_6_classes.h5'
     # model_name = 'resnet_model_12_classes.h5'
+    model_name = 'resnet_model_12_classes_new.h5'
     model = load_model(model_name)
     print('Model successfully loaded')
 
@@ -71,23 +73,27 @@ def getDetails(plant_name):
     )
     cursor = conn.cursor()
     cursor.execute("select * from plants where common_name='"+plant_name.lower()+"';")
-    rows = cursor.fetchall()
+    details = cursor.fetchall()
     columns = [desc[0] for desc in cursor.description]
     columns = [col_name.replace("_", " ").capitalize() for col_name in columns]
+    cursor.execute("select feature from characteristics where common_name='"+plant_name.lower()+"';")
+    features = cursor.fetchall()
+    print(columns)
+    features = [item[0] for item in features]
+    print(features)
     json_data = {}
-    rows = rows[0]
+    details = details[0]
     for i, column in enumerate(columns):
         if i==0:
-            json_data[column] = rows[i].replace("_", " ").capitalize()
+            json_data[column] = details[i].replace("_", " ").capitalize()
         else:
-            json_data[column]=rows[i]
-    print(json_data)
-    for item in json_data:
-        print(item)
-        print(json_data.get(item))             
+            json_data[column]=details[i]
+    json_data['Characteristics'] = features
+    print(json_data)          
     cursor.close()
     conn.close()
     return render_template('details_page.html', data = json_data)
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
+    # app.run(debug=True)
